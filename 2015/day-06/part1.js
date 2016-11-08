b53d1893aa962d11d583c4e8fcf80d1b866ff928
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-11-08 22:32:55 +0100

    06 - 1

--- 2015/day-06/part1.js
+++ 2015/day-06/part1.js
@@ -1,25 +1,53 @@
-'use strict'
-
 import fs from 'fs'
 
-let input = fs.readFileSync('test.txt', 'utf8').split('\n')
+let input = fs.readFileSync('input.txt', 'utf8').trim().split('\n')
 
-var patt = new RegExp(/^[0-9,0-9]+$/)
-let coordinates = []
-coordinates.push('hi')
+let grid = []
 
+for(var i = 0; i < 1000; i++) {
+    grid[i] = []
 
-/*let i = 0
-let answer = input.map(instruction => {
-    instruction = instruction.split(' ')
-    instruction.map(coord => {
-        if (patt.test(coord)) {
-            coordinates[i].push(coord)
+    for (var y = 0; y < 1000; y++) {
+        grid[i][y] = {
+            status: 0
         }
-    })
+    }
+}
 
-    //console.log(instruction)
-    i++
-})*/
+let regex = /^(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)$/
 
-console.log(coordinates)
+for (var i = 0; i < input.length; i++) {
+    let operation = input[i].match(regex)
+
+    let x1 = +operation[2]
+    let y1 = +operation[3]
+    let x2 = +operation[4]
+    let y2 = +operation[5]
+    operation = operation[1]
+
+    for(var i1 = x1; i1 <= x2; i1++) {
+        for(var i2 = y1; i2 <= y2; i2++) {
+            switch (operation) {
+                case 'turn on':
+                    grid[i1][i2].status = 1
+                    break
+                case 'turn off':
+                    grid[i1][i2].status = 0
+                    break
+                case 'toggle':
+                    grid[i1][i2].status ^= 1
+                    break
+            }
+        }
+    }
+}
+
+let lits = 0
+
+for (var i = 0; i < grid.length; i++) {
+    for(var y = 0; y < grid[i].length; y++) {
+        lits += grid[i][y].status
+    }
+}
+
+console.log(lits)
